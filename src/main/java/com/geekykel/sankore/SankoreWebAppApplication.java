d6577931ffd3dd6330db07690d6ee71968148a9b
Kelvinator07 <kelvinator4leo@gmail.com>
2020-03-02 23:55:41 +0100

    Spring Web App

---
+++ src/main/java/com/geekykel/sankore/SankoreWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.geekykel.sankore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SankoreWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SankoreWebAppApplication.class, args);
+	}
+
+}
