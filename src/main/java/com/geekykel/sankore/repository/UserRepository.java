170be8fa429d304cdce09f76ca6a1c40addf83ee
Kelvinator07 <kelvinator4leo@gmail.com>
2020-03-03 03:58:00 +0100

    Updated User Repository

--- src/main/java/com/geekykel/sankore/repository/UserRepository.java
+++ src/main/java/com/geekykel/sankore/repository/UserRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by Kelvin on 3/02/2020
  */
-public interface UserRespository extends JpaRepository<User, Long> {
-	
-	//@Query(value = "SELECT type, count(type) FROM location GROUP BY type")
-	//List<Object[]> findTypeAndTypeCount();
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
